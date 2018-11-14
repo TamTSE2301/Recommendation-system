759dff35cb215257066e7c0495329bcea1d79181
Thanh-Tam NGUYEN <40611425+TamTSE2301@users.noreply.github.com>
2018-11-15 00:44:05 +0100

    Update Surprise Library Check - Similarity matrix.py

--- Surprise Library Check - Similarity matrix.py
+++ Surprise Library Check - Similarity matrix.py
@@ -7,59 +7,35 @@
 #Libraires used:
 import pandas as pd
 import numpy as np
-
-#This is a random dataset that needs predictions
 data = pd.read_csv("data.csv",index_col = 0)
 data.head(10)
 
 
 # # SURPRISE LIBRARY
 
-# In[2]:
-
-
 from surprise import Reader, Dataset, trainset
 from surprise.prediction_algorithms import KNNWithMeans
 from surprise import Reader, Dataset, trainset
-
-
-# In[3]:
-
 
 #Build trainset
 data['User']=data.index
 trainset = pd.melt(data,id_vars='User',value_vars=data.columns.drop('User').tolist()).dropna()
 data = data.drop('User',axis=1)
 trainset.columns = ['User','Item','Value']
-
-
-# In[6]:
-
-
 reader = Reader(rating_scale=(-1000,1000)) #Change to max and min of non-NA data
 TRAIN = Dataset.load_from_df(trainset[['User','Item','Value']], reader=reader)
 trainset = TRAIN.build_full_trainset()
-
-
-# In[7]:
 
 
 sim_options = sim_options={'name':'pearson','min_support':2,'user_based':True}
 algo = KNNWithMeans(min_k=2,k=10,sim_options=sim_options)
 algo.fit(trainset)
 
-
-# In[10]:
-
-
 #Pearson corr of U1 
 algo.sim[0]
 
 
 # # Manual Computation
-
-# In[12]:
-
 
 from scipy.stats.stats import pearsonr
 
